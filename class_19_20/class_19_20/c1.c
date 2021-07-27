de664ca10732fdb6ecec0128fc68e493f5640fb5
WilliamConnor <williamconnerlc@hotmail.com>
2021-07-27 10:04:46 +0800

    初识指针

---
+++ class_19_20/class_19_20/c1.c
@@ -0,0 +1,58 @@
+#define _crt_secure_no_warnings 1
+#include<stdio.h>
+
+////模拟strlen函数方法①计数器②递归③指针
+////指针法 模拟strlen函数
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	char* end = str;
+//	while (*end != '\0')
+//	{
+//		end++;//end自加直到\0，之前的元素个数即是长度
+//	}
+//	return end - start;//返回两个指针中间元素，也即字符串长度
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+
+
+////数组 利用 指针 修改值
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = i+1;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
+
+
+////指针数组——存放指针的数组
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int* arr[3] = { &a,&b,&c };//指针数组初始化
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d ", *(arr[i]));
+//	}
+//	return 0;
+//}
